59010d783dd121193346d6e385e7186a59348def
David Lange <lange@finiam.com>
2023-11-22 15:35:57 +0000

    fix deploy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,3 @@
-import { formatFixed } from "@ethersproject/bignumber";
-import { uint256 } from "starknet";
-
-export function format(val: any, key: string, decimals: number) {
-  if (!val) return null;
-
-  const asBN = uint256.uint256ToBN(val[key]);
-  return formatFixed(asBN.toString(), decimals);
-}
-
 export function truncate(val: string) {
   return `${val.substring(0, 5)}...${val.slice(-2)}`;
 }
